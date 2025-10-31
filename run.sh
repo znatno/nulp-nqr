b3260c2e5d6d1d79971fba1cf147c817207cfcf7
Ivan Bohun <ivanbohuna@gmail.com>
2025-10-31 17:35:17 +0200

    upd?

---
+++ run.sh
@@ -0,0 +1,19 @@
+# backend
+node ./backend/dist/index.js
+
+# DB
+brew services start postgresql@14
+psql -h localhost -p 5432 -U postgres -d nulp_nqf_register
+
+# clean DB
+dropdb -U postgres nulp_nqf_register
+createdb -U postgres nulp_nqf_register
+npx prisma migrate dev --name init
+npx prisma db seed
+
+# Prisma (DB Studio)
+npx prisma db seed
+npx prisma studio
+
+# frontend
+npm run dev
